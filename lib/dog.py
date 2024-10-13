cf92e05c7ebcf0af9f1a2aa29ea05c5e4366e1c2
William Gitau <dontysh@gmail.com>
2024-10-13 23:00:10 +0300

    Passes all tests

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,39 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    APPROVED_BREEDS = APPROVED_BREEDS
+
+    def __init__(self, name = None, breed = None):
+        # Validate name
+        self._name = None
+        if name is not None:
+            self.name = name  # Use property setter to validate
+
+        # Validate breed
+        self._breed = None
+        if breed is not None:
+            self.breed = breed  # Use property setter to validate
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if value == "":
+            print("Name must be string between 1 and 25 characters.")
+        elif not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value.title()
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value not in Dog.APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = value
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,40 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    APPROVED_JOBS = APPROVED_JOBS
+
+    def __init__(self, name = None, job = None):
+        # Validate name
+        self._name = None
+        if name is not None:
+            self.name = name  # Use property setter to validate
+
+        # Validate job
+        self._job = None
+        if job is not None:
+            self.job = job  # Use property setter to validate
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if value == "":
+            print("Name must be string between 1 and 25 characters.")
+        elif not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value.title()
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value not in Person.APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = value
+            
